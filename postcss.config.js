797d5ee011bd580eefc4603df5c12aa000185a26
Stefan <insertinsight@gmail.com>
2020-09-23 14:24:51 +0200

    init repo

---
+++ postcss.config.js
@@ -0,0 +1,6 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable no-undef */
+const tailwindcss = require('tailwindcss')
+module.exports = {
+	plugins: [tailwindcss('./tailwind.js'), require('autoprefixer')]
+}
